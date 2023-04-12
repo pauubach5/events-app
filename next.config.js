e7fe6312c30b3e1983ab8e35172561cf250a1197
pauubach <p.ubach1999@gmail.com>
2023-04-12 18:51:39 +0200

    next config changed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'wembleypark.com']
-  }
 }
 
 module.exports = nextConfig
